9977aef471eac5dc32cc1613b25fd1d88c75efcb
helderdsa <helder_dsa@hotmail.com>
2026-02-04 09:59:30 -0300

    Adiciona componente SobreNos e SobreNosCard com conteúdo e imagens

---
+++ app/components/SobreNos.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import { Icon } from '@iconify/react';
+import { SobreNosCard } from './SobreNosCard';
+
+export function SobreNos() {
+  return (
+    <section className="py-16 bg-[#F2F4F5]">
+      <div className="container mx-auto">
+      {/* título */}
+        <div className="inline-flex items-center gap-2 px-4 py-2 rounded-full text-sm font-medium text-text-darkgray">
+          <span className="w-8 h-1 bg-secondary rounded-full"></span>
+          <span className="text-xl font-bold text-primary">Sobre nós</span>
+        </div>
+        <div className="grid grid-cols-12 mt-8 px-4">
+          <div className="col-span-6">
+            <h2 className="text-primary text-5xl font-bold">Tradição, compromisso <br /> <span className="text-secondary">e excelência jurídica</span></h2>
+            <p className="text-[#6A80B0] mt-4 text-justify">Somos um escritório que pratica a advocacia com visão estratégica e inovação para que os nossos resultados apresentem sempre melhoria contínua em todos os âmbitos desde os processos operacionais aos gerenciais. Com foco em resultados, desenvolvemos soluções customizadas para os servidores públicos, com ênfase nos servidores da educação</p>
+            <div className='text-[#6A80B0] flex items-center mt-6'>
+              <Icon icon="lets-icons:check-fill" className="inline-block text-secondary w-9 h-9 mr-2" />
+              Professores Ativos e Aposentados
+            </div>
+            <div className='text-[#6A80B0] flex items-center mt-6'>
+              <Icon icon="lets-icons:check-fill" className="inline-block text-secondary w-9 h-9 mr-2" />
+              Servidores públicos
+            </div>
+          </div>
+          <div className="col-span-6 flex justify-center items-center">
+            <div className="h-96 w-96 rounded-full relative">
+              <Image src="/fotoEscritorio.png" alt="escritorio" layout="fill" objectFit="cover" className="rounded-full" />
+              <div className="h-56 w-56 rounded-full relative -right-64 bottom-8">
+                <Image src="/fotoAssinando.png" alt="assinando" layout="fill" objectFit="cover" className="rounded-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-24 flex gap-40'>
+          <SobreNosCard titulo='Transparencia' texto='Mais de 15 mil ações procedentes' icone="octicon:law-16" />
+
+          <SobreNosCard titulo='Atuação' texto='Atuação em seis Estados brasileiros' icone="game-icons:brazil" />
+
+          <SobreNosCard titulo='Atuação' texto='Quase 10 anos de atuação na área jurídica' icone="fa7-solid:hourglass-2" />
+        </div>
+
+      </div>
+    </section>
+  );
+}
---
+++ app/components/SobreNosCard.tsx
@@ -0,0 +1,26 @@
+import { Icon } from "@iconify/react";
+
+interface SobreNosCardProps {
+  titulo: string;
+  icone: string;
+  texto: string;
+}
+
+export function SobreNosCard({ titulo, icone, texto }: SobreNosCardProps) {
+  return (
+    <div className="flex items-center">
+      <div className="flex items-center border border-text-color shadow-text-color shadow-2xl rounded-lg w-70">
+          <span className="bg-secondary h-20 w-1 rounded-r-full" />
+          <div className="p-4">
+            <div className="flex items-center justify-between mb-2">
+              <div className="text-primary text-sm">{titulo}</div>
+              <Icon icon={icone} className="inline-block text-secondary w-10 h-10 p-0 m-0" />
+            </div>
+            <h2 className="text-primary font-bold">{texto}</h2>
+
+          </div>
+        </div>
+    </div>
+    
+  )
+}
